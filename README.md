de06ec41bd0f9a4da0ffb0022e3757ee48077b2e
Juan Antonio Hernández Cánovas <juanantoniohernandezcanovas@gmail.com>
2021-08-15 14:45:19 +0100

    Create repo structure and cmake file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# dx12_renderer
-A DirectX 12 multi-threaded rendering system
+# DirectX 12 Renderer
+
+A DirectX 12 multi-threaded rendering system. I made this system to learn about DirectX 12 and I use it whenever I want to test or implement a graphics technique or application.
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+#include <d3dx12.h>
+
+#include "assimp/Importer.hpp"
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow )
+{
+    return 0;
+}
